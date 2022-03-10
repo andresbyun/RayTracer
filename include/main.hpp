f6216de541c7f1500444252ab491d4694dac2824
Andres Byun <adb0202@hotmail.com>
2022-03-10 15:41:21 -0800

    Initial file setup

---
+++ include/main.hpp
@@ -0,0 +1,6 @@
+#ifndef __MAIN__
+
+/* Function definitions */
+int main(int argc, char** argv);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <main.hpp>
+#include <iostream>
+
+/* Using statements */
+using std::cout;
+using std::endl;
+
+/* Main function */
+/* TODO: make it do something */
+int main(int argc, char** argv) {
+	cout << "Hello, RayTracer" << endl;
+	return 0;
+}
