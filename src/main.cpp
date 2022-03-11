72ea8a55642fcd0241a8f098bfcced811880af36
Andres Byun <adb0202@hotmail.com>
2022-03-10 16:07:38 -0800

    Add GLM to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
+/* Include statements */
 #include <main.hpp>
 #include <iostream>
+#include <vector>
+#include <glm/glm/glm.hpp>
 
 /* Using statements */
 using std::cout;
 using std::endl;
+using std::vector;
+using glm::mat4;
+using glm::vec4;
 
 /* Main function */
 /* TODO: make it do something */
